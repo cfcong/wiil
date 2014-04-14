f64d4647a82e29927fa2b3a3c6f4e1ac5a084797
rexyeung <rex.627@gmail.com>
2014-04-14 00:20:02 -0700

    Added images to front page

---
+++ wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
@@ -0,0 +1,11 @@
+<div id="brands" class="brands">
+	<div id="brand_1" class="brand">
+		<img class="brand_img" src="/drupal7/sites/default/images/SwissOParBannerHovered.jpg">
+	</div>
+	<div id="brand_2" class="brand">
+		<img class="brand_img" src="/drupal7/sites/default/images/SuisseAbsolueBannerHovered.jpg">
+	</div>
+	<div id="brand_3" class="brand">
+		<img class="brand_img" src="/drupal7/sites/default/images/JoJoBannerHovered.jpg">
+	</div>
+</div>
