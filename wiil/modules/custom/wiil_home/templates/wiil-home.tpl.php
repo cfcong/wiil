f987f1f5c69005ede67c55b7ee0071453550b3d2
rexyeung <rex.627@gmail.com>
2014-04-14 21:53:13 -0700

    CSS front page changes

--- wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
+++ wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
@@ -1,11 +1,11 @@
 <div id="brands" class="brands">
 	<div id="brand_1" class="brand">
-		<img class="brand_img" src="/drupal7/sites/default/images/SwissOParBannerHovered.jpg">
+		<img class="brand_img" src="sites/default/images/SwissOParBannerHovered.jpg">
 	</div>
 	<div id="brand_2" class="brand">
-		<img class="brand_img" src="/drupal7/sites/default/images/SuisseAbsolueBannerHovered.jpg">
+		<img class="brand_img" src="sites/default/images/SuisseAbsolueBannerHovered.jpg">
 	</div>
 	<div id="brand_3" class="brand">
-		<img class="brand_img" src="/drupal7/sites/default/images/JoJoBannerHovered.jpg">
+		<img class="brand_img" src="sites/default/images/JoJoBannerHovered.jpg">
 	</div>
 </div>
