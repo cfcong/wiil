b7c0ba1004c03e64fcf15e9dfc83f750ea06c4a4
rexyeung <rex.627@gmail.com>
2014-04-15 22:32:07 -0700

    Added header/footer blocks

---
+++ wiil/modules/custom/wiil_home/templates/wiil-footer.tpl.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>HTML Generator Sample Page</title>
+	</head>
+	<body>
+		<p>
+			ADDRESS</p>
+		<p>
+			Room 1702, 17/F., Qualipak Tower<br />
+			121-122 Connaught Road West<br />
+			Hong Kong<br />
+			<br />
+			HOURS</p>
+		<p>
+			Monday - Friday<br />
+			9:30AM - 6:30PM<br />
+			Saturday<br />
+			Closed<br />
+			Sunday<br />
+			Closed</p>
+		<p>
+			CONTACT</p>
+		<p>
+			Tel: +852 1234 5678<br />
+			Email: support@wingedinternational.com</p>
+		<p>
+			FIND US ON:</p>
+		<img src="sites/default/images/WingedLogo300px.png">
+	</body>
+</html>
---
+++ wiil/modules/custom/wiil_home/templates/wiil-header.tpl.php
@@ -0,0 +1,1 @@
+<div id="wiil-header"></div>
