df5842de5311f23f422ee0d7c399d0a82296f3ad
rexyeung <rex.627@gmail.com>
2014-04-18 12:46:17 -0700

    added new images on template

--- wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
+++ wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
@@ -3,6 +3,6 @@
 		<img class="brand_img" src="sites/default/images/SuisseAbsolueBannerHovered_600px.jpg">
 	</div>
 	<div id="brand_2" class="brand">
-		<img class="brand_img" src="sites/default/images/JojoBannerHovered_600px.jpg">
+		<img class="brand_img" src="sites/default/images/JojoBannerHovered_600px.jpgw">
 	</div>
 </div>
