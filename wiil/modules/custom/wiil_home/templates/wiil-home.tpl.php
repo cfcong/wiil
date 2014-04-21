346fbc84ebff641c8f71b5fdb035967a7d7817ee
Colin Fcong <cfcong@gmail.com>
2014-04-20 22:07:48 -0700

    fixed naming convention

--- wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
+++ wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
@@ -1,13 +1,13 @@
 <div id="brands" class="brands">
 	<div id="brand_1" class="brand">
 		<img class="brand_img" src="sites/default/images/SuisseAbsolueBannerHovered_600px.jpg">
-    <div id="tab_img" class="tab">
+    <div id="logo_name" class="logo">
 	   <img class="logo_img" src="sites/default/images/SuisseAbsolueLogoCLear.png">
     </div>
   </div>
 	<div id="brand_2" class="brand">
 		<img class="brand_img" src="sites/default/images/JojoBannerHovered_600px.jpg">
-    <div id="tab_img" class="tab">
+    <div id="logo_name" class="logo">
       <img class="logo_img" src="sites/default/images/JoJoLogoCLear.png">
     </div>
   </div>
