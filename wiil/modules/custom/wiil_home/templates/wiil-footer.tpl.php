a0849cbe5053b894720bca28c7b841627d132340
Colin Fcong <cfcong@gmail.com>
2014-04-20 22:06:20 -0700

    fixed footer to display properly

--- wiil/modules/custom/wiil_home/templates/wiil-footer.tpl.php
+++ wiil/modules/custom/wiil_home/templates/wiil-footer.tpl.php
@@ -1,30 +1,30 @@
 <div id="wrapper">
-	<div id="address">ADDRESS<br>
-		<br>
-		Room 1702, 17/F., Qualipak Tower<br>
-		121-122 Connaught Road West<br>
-		Hong Kong<br>
-		<br>
-		香港幹諾道西121-122號<br>
-		確利達中心17樓1701室<br>
-	</div>
-	<div id="hours">HOURS<br>
-		<br>
-		Monday - Friday<br>
-		9:30AM - 6:30PM<br>
-		Saturday<br>
-		Closed<br>
-		Sunday<br>
-		Closed<br>
-		<br>
-	</div>
-	<div id="contact">CONTACT<br>
-		<br>
-		Tel: +852 1234 5678<br>
-		Email: support@wingedinternational.com<br>
-		<br>
-		FIND US ON:
-		<br>
-	</div>
-	<!-- <img src="sites/default/images/WingedLogo300px.png"> -->
+		<!--<img id="footer_logo" src="sites/default/images/WingedLogo300px.png">-->
+		<div class="address">ADDRESS<br>
+			<br>
+			Room 1702, 17/F., Qualipak Tower<br>
+			121-122 Connaught Road West<br>
+			Hong Kong<br>
+			<br>
+			香港幹諾道西121-122號<br>
+			確利達中心17樓1701室<br>
+		</div>
+		<div class="hours">HOURS<br>
+			<br>
+			Monday - Friday<br>
+			9:30AM - 6:30PM<br>
+			Saturday<br>
+			Closed<br>
+			Sunday<br>
+			Closed<br>
+			<br>
+		</div>
+		<div class="contact">CONTACT<br>
+			<br>
+			Tel: +852 1234 5678<br>
+			Email: support@wingedinternational.com<br>
+			<br>
+			FIND US ON:
+			<br>
+		</div>
 </div>
