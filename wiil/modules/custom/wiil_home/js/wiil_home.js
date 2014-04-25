aa8280469e40b7e4405279efde3926fafc379801
rexyeung <rex.627@gmail.com>
2014-04-25 01:30:31 -0700

    Updates

--- wiil/modules/custom/wiil_home/js/wiil_home.js
+++ wiil/modules/custom/wiil_home/js/wiil_home.js
@@ -1,6 +1,14 @@
 (function ($) {
   Drupal.behaviors.wiil_home = {
     attach: function (context, settings) {
+    	$('.brand_img').hover(
+    		function() {
+    			$(this).next().attr('style', 'background-color: transparent');
+    		},
+    		function() {
+    			$(this).next().attr('style', 'background-color: lightgrey');
+    		}
+    	);
     }
   };
 })(jQuery);
