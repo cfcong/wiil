48785f95d633792e621061093ba552b09df10918
rexyeung <rex.627@gmail.com>
2014-04-28 22:08:25 -0700

    Fixed home brand menu and added google analytics

--- wiil/modules/custom/wiil_home/js/wiil_home.js
+++ wiil/modules/custom/wiil_home/js/wiil_home.js
@@ -1,14 +1,34 @@
 (function ($) {
   Drupal.behaviors.wiil_home = {
     attach: function (context, settings) {
-    	$('.brand_img').hover(
+    	$('.brand').hover(
     		function() {
-    			$(this).next().attr('style', 'background-color: transparent');
+    			$(this).attr('style', 'background-color: transparent');
+                if($(this).attr('id') == 'brand_1') {
+                    $('#brand_1 .brand_menu').attr('style' , 'visibility: inherit');
+                    $('#logo_inspira').attr('style', 'background-color: transparent');
+                }
+                else if($(this).attr('id') == 'brand_2') {
+                    $('#brand_2 .brand_menu').attr('style' , 'visibility: inherit');
+                    $('#logo_jojo').attr('style', 'background-color: transparent');
+                }
     		},
     		function() {
-    			$(this).next().attr('style', 'background-color: lightgrey');
+    			$(this).attr('style', 'background-color: lightgrey');
+                $('.brand_menu').attr('style' , 'visibility: hidden');
+                $('#logo_inspira').attr('style', 'background-color: lightgrey');
+                $('#logo_jojo').attr('style', 'background-color: lightgrey');
     		}
     	);
     }
   };
 })(jQuery);
+
+/* Google Analytics */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-50386548-1', 'gotpantheon.com');
+ga('send', 'pageview');
--- wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
+++ wiil/modules/custom/wiil_home/templates/wiil-home.tpl.php
@@ -1,13 +1,29 @@
 <div id="brands" class="brands">
 	<div id="brand_1" class="brand">
 		<img class="brand_img" src="sites/default/images/SuisseAbsolueBannerHovered_600px.jpg">
-    <div id="logo_name" class="logo">
+    <div class='brand_menu'>
+      <ul>
+        <li><a href="http://jojo-haircosmetics-shop.de/jojo/company?lang=en" target="_blank">ABOUT</a></li>
+        <li><a href="http://jojo-haircosmetics-shop.de/jojo/products?lang=en" target="_blank">PRODUCTS</a></li>
+        <li><a href="http://jojo-haircosmetics-shop.de/jojo/?lang=en" target="_blank">PROMOTIONS</a></li>
+        <li><a href="http://jojo-haircosmetics-shop.de/jojo/?lang=en" target="_blank">STORE LOCATOR</a></li>
+      </ul>
+    </div>
+    <div id="logo_inspira" class="logo">
 	   <img class="logo_img" src="sites/default/images/SuisseAbsolueLogoCLear.png">
     </div>
   </div>
 	<div id="brand_2" class="brand">
 		<img class="brand_img" src="sites/default/images/JojoBannerHovered_600px.jpg">
-    <div id="logo_name" class="logo">
+    <div class='brand_menu'>
+    <ul>
+      <a href="http://www.inspira-cosmetics.de/en/about_us/" target="_blank">ABOUT</a>
+      <a href="http://www.inspira-cosmetics.de/en/brands/suisse_absolue/" target="_blank">PRODUCTS</a>
+      <a href="http://www.inspira-cosmetics.de/en/brands/suisse_absolue/ " target="_blank">PROMOTIONS</a>
+      <a href="http://www.inspira-cosmetics.de/en/brands/suisse_absolue/ " target="_blank">STORE LOCATOR</a>
+    </ul>
+    </div>
+    <div id="logo_jojo" class="logo">
       <img class="logo_img" src="sites/default/images/JoJoLogoClear.png">
     </div>
   </div>
