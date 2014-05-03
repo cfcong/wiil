5a298d032429ef449f85a4d0acc22ad34f1769af
Malffej <jeff.lam1990@gmail.com>
2014-05-03 14:50:37 -0700

    Working on Jojo brand page, updates as of 5/3/2014 2:50PM PST

---
+++ wiil/modules/custom/wiil_brands_jojo/js/wiil_brands_jojo.js
@@ -0,0 +1,7 @@
+(function ($) {
+	Drupal.behaviors.wiil_brands_jojo = {
+		attach: function (context, settings){
+				//alert("wiil_brands_jojo.js works!");
+		}
+	}
+}(jQuery));
---
+++ wiil/modules/custom/wiil_brands_jojo/templates/wiil-brands-jojo.tpl.php
@@ -0,0 +1,25 @@
+<div id="banner" class="banner">
+	<img class="banner_img" src="sites/default/images/jojo_brands/JOJO-Page-Banner.jpg">
+	<img class="logo_img" src="sites/default/images/jojo_brands/JOJO-Page-Haircosmetics-Logo.png">
+</div>
+
+
+
+<div id="wrapper">
+	<div class="aboutTitle">ABOUT
+	</div>
+	<div class="aboutContent">
+		JOJO HAIRCOSMETICS IS A DYNAMIC<br>
+		FUTURE ORIENTED COSMETIC MANUFACTURER.<br>
+		FOUND 1983 BY JOSCHI POZSAR<br>
+		<br>
+		THE COMPANY PRODUCES COLOR, STYLING,<br>
+		HAIRCARE AND PERM PRODUCTS EXCLUSIVELY<br>
+		FOR THE HAIRDRESSER.<br>
+		<br>
+		OUR PRODUCTS ARE TESTED FOR QUALITY AND<br>
+		APPLICATION SUITABILITY AND UNDERGO A<br>
+		PRACTICE TEST AT EXPERIENCED HAIRDRESSERS<br>
+		BEFORE THEY ARE BROUGHT ON THE MARKET.<br>
+	</div>
+</div>
