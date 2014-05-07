3015424db39c5a72fadbdcaa35c868cd4ae2cc2b
Malffej <jeff.lam1990@gmail.com>
2014-05-07 16:03:03 -0700

    Removed unneccesary modules used for a previous jojo brands slideshow build. New JS file with basic css included.

--- wiil/modules/custom/wiil_brands_jojo/templates/wiil-brands-jojo.tpl.php
+++ wiil/modules/custom/wiil_brands_jojo/templates/wiil-brands-jojo.tpl.php
@@ -1,19 +1,61 @@
-<div id="banner" class="banner">
-	<img class="banner_img" src="/sites/default/images/jojo_brands/JOJO-Page-Banner.jpg">
-	<img class="logo_img" src="/sites/default/images/jojo_brands/JOJO-Page-Haircosmetics-Logo.png">
-</div>
-
-
-
-<div id="wrapper">
-	<div class="aboutTitle">
-		<img class="about_banner" src="/sites/default/images/jojo_brands/JOJO-Page-About-Logo.png">
+<div class="wrapper">
+	<h1>Hairpure</h1>
+	<div class="jcarousel-wrapper">
+		<div class="jcarousel">
+			<ul>
+				<li><img src="http://icons.iconarchive.com/icons/vargas21/aquave-metal/128/Sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/wineass/ios7-redesign/128/Sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/3xhumed/cryonic-folder/128/Folder-Blank-sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Pictures-Canon-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/treetog/junior/128/camera-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/robsonbillponte/happy-holidays/128/Pictures-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Pictures-Nikon-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/apathae/satellite/128/1-Pictures-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/treetog/junior/128/folder-blue-music-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Graphics-Painting-icon.png"></li>
+			</ul>
+		</div>
+		<a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+		<a href="#" class="jcarousel-control-next">&rsaquo;</a>
 	</div>
-	<div class="aboutContent">
-		<img class="about_img" src="/sites/default/images/jojo_brands/JOJO-Page-About-Text1.png"><br>
-		<br>
-		<img class="about_img" src="/sites/default/images/jojo_brands/JOJO-Page-About-Text2.png"><br>
-		<br>
-		<img class="about_img" src="/sites/default/images/jojo_brands/JOJO-Page-About-Text3.png"><br>
+
+	<h1>Hairpure Style</h1>
+	<div class="jcarousel-wrapper">
+		<div class="jcarousel">
+			<ul>
+				<li><img src="http://icons.iconarchive.com/icons/vargas21/aquave-metal/128/Sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/wineass/ios7-redesign/128/Sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/3xhumed/cryonic-folder/128/Folder-Blank-sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Pictures-Canon-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/treetog/junior/128/camera-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/robsonbillponte/happy-holidays/128/Pictures-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Pictures-Nikon-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/apathae/satellite/128/1-Pictures-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/treetog/junior/128/folder-blue-music-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Graphics-Painting-icon.png"></li>
+			</ul>
+		</div>
+		<a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+		<a href="#" class="jcarousel-control-next">&rsaquo;</a>
+	</div>
+
+		<h1>Colorpure</h1>
+	<div class="jcarousel-wrapper">
+		<div class="jcarousel">
+			<ul>
+				<li><img src="http://icons.iconarchive.com/icons/vargas21/aquave-metal/128/Sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/wineass/ios7-redesign/128/Sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/3xhumed/cryonic-folder/128/Folder-Blank-sample-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Pictures-Canon-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/treetog/junior/128/camera-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/robsonbillponte/happy-holidays/128/Pictures-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Pictures-Nikon-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/apathae/satellite/128/1-Pictures-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/treetog/junior/128/folder-blue-music-icon.png"></li>
+				<li><img src="http://icons.iconarchive.com/icons/itzikgur/my-seven/128/Graphics-Painting-icon.png"></li>
+			</ul>
+		</div>
+		<a href="#" class="jcarousel-control-prev">&lsaquo;</a>
+		<a href="#" class="jcarousel-control-next">&rsaquo;</a>
 	</div>
 </div>
